c5323e838cd4dc8f514d717d0ae5102ffdfdd5c8
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-01 21:31:36 +0700

    revert to default

--- src/detector.c
+++ src/detector.c
@@ -268,25 +268,25 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
 
         //if (i % 1000 == 0 || (i < 1000 && i % 100 == 0)) {
         //if (i % 100 == 0) {
-        //if (i >= (iter_save + 1000) || i % 1000 == 0) {
-        if (i % 100 == 0) {
+        if (i >= (iter_save + 1000) || i % 1000 == 0) {
             iter_save = i;
+#ifdef GPU
+            if (ngpus != 1) sync_nets(nets, ngpus, 0);
+#endif
+            char buff[256];
+            sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
+            save_weights(net, buff);
+        }
+
+        if (i >= (iter_save_last + 100) || i % 100 == 0) {
             iter_save_last = i;
 #ifdef GPU
             if (ngpus != 1) sync_nets(nets, ngpus, 0);
 #endif
-            
             char buff[256];
             sprintf(buff, "%s/%s_last.weights", backup_directory, base);
             save_weights(net, buff);
-            
-            if(i%2000==0) {
-                char buff[256];
-                sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
-                save_weights(net, buff);
-            }
         }
-        
         free_data(train);
     }
 #ifdef GPU
