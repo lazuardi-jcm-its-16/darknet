2b30f0ed9221cdbd093a7c93496d16f8e591d76d
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-03 01:01:51 +0700

    truth bbds

--- src/detector.c
+++ src/detector.c
@@ -1214,7 +1214,7 @@ float validate_detector_map_bulk(char *datacfg, char *cfgfile, char *weightfile,
             char* buff_file_name = (char*)calloc(8192, sizeof(char));
             sprintf(buff_file_name, "/tesis/detect%s", path);
             draw_detections_v3(buf[image_index], dets, nboxes, thresh, names, alphabet, l.classes, 0);
-            save_image(buf[image_index], buff_file_name);
+            //save_image(buf[image_index], buff_file_name);
 
             // difficult
             box_label *truth_dif = NULL;
