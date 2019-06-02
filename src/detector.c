3565d4ca458e7669952be268da58468689b2c994
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-02 23:55:13 +0700

    test

--- src/detector.c
+++ src/detector.c
@@ -1192,11 +1192,13 @@ float validate_detector_map_bulk(char *datacfg, char *cfgfile, char *weightfile,
             if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
             
             printf("%s\n",(char*)"masuk bro");
-            /*
+            
             char buff_file_name[1024];
             sprintf(buff_file_name, "/tesis/detect/%s", basename(path));
-            draw_detections_v3(buf[image_index], dets, nboxes, thresh, names, alphabet, l.classes, 0);
             printf("%s\n",buff_file_name);
+            
+            /*
+            draw_detections_v3(buf[image_index], dets, nboxes, thresh, names, alphabet, l.classes, 0);
             save_image(buf[image_index], buff_file_name);*/
 
             char labelpath[4096];
