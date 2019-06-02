eac991db13aaaea3994238137f6477e8a6b342ad
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-02 23:30:10 +0700

    detect bulk

--- src/darknet.c
+++ src/darknet.c
@@ -474,7 +474,7 @@ int main(int argc, char **argv)
         run_detector(argc, argv);
     } else if (0 == strcmp(argv[1], "detect")){
         float thresh = find_float_arg(argc, argv, "-thresh", .24);
-		int ext_output = find_arg(argc, argv, "-ext_output");
+        int ext_output = find_arg(argc, argv, "-ext_output");
         char *filename = (argc > 4) ? argv[4]: 0;
         test_detector("cfg/coco.data", argv[2], argv[3], filename, thresh, 0.5, 0, ext_output, 0, NULL);
     } else if (0 == strcmp(argv[1], "cifar")){
