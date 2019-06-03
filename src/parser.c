66b328bf6d2b0681b513c39ec2130f608a137276
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-04 01:50:19 +0700

    list map and loss

--- src/parser.c
+++ src/parser.c
@@ -1160,28 +1160,28 @@ void save_loss(list *list_loss, char* filename) {
     FILE *fp = fopen(filename, "ab");
     if(!fp) file_error(filename);
     
-    //_lossAcc **array_of_loss = (_lossAcc **)list_to_array(list_loss);
+    _lossAcc **array_of_loss = (_lossAcc **)list_to_array(list_loss);
     printf("size loss: %d\n",list_loss->size);
  
-    char cr = 0x0d;
-    char lf = 0x0a;
-        
     for(int i=0; i<list_loss->size; i++) {
-        _lossAcc *loss = (_lossAcc*)list_pop(list_loss);
+        char cr = 0x0d;
+        char lf = 0x0a;
+        int iter = array_of_loss[i]->iterBatch;
+        float avg_loss = array_of_loss[i]->avgLoss;
+        float max_img_loss = array_of_loss[i]->maxImgLoss;
         
-        int iter = loss->iterBatch;
-        float avg_loss = loss->avgLoss;
-        float max_img_loss = loss->maxImgLoss;
-        
-        printf("%d %.5f %.5f\n",loss->iterBatch,loss->avgLoss,loss->maxImgLoss);
+        printf("%d %.5f %.5f\n",array_of_loss[i]->iterBatch,array_of_loss[i]->avgLoss,array_of_loss[i]->maxImgLoss);
 
         fwrite(&cr, sizeof(char), 1, fp);
         fwrite(&lf, sizeof(char), 1, fp);
         fwrite(&iter, sizeof(int), 1, fp);
         fwrite(&avg_loss, sizeof(float), 1, fp);
         fwrite(&max_img_loss, sizeof(float), 1, fp);
-        
-        ;
+    }
+    
+    int size_list = list_loss->size;
+    for(int i=0; i<size_list; i++) {
+        list_pop(list_loss);
     }
     
     fclose(fp);
