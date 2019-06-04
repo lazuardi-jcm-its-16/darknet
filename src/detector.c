2d512ea11dd4c92a9fe59d9cdc02899726b0efe4
Arif F Lazuardi <lazuardi16@mhs.ee.its.ac.id>
2019-06-04 10:34:38 +0700

    dynamic iou threshold

--- src/detector.c
+++ src/detector.c
@@ -22,7 +22,7 @@ int check_mistakes;
 
 static int coco_ids[] = { 1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90 };
 
-void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, int ngpus, int clear, int dont_show, int calc_map, int mjpeg_port, int show_imgs)
+void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, int ngpus, int clear, int dont_show, int calc_map, int mjpeg_port, int show_imgs,float iou_thresh=.5f)
 {
     list *list_mAP = make_list();
     _mAP *mAP = 0;
@@ -30,7 +30,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     list *list_loss = make_list();
     _lossAcc *lossAcc = 0;
     
-    float tresh_IoU = 0.75f;
+    float tresh_IoU = iou_thresh;
     
     list *options = read_data_cfg(datacfg);
     char *train_images = option_find_str(options, "train", "data/train.txt");
